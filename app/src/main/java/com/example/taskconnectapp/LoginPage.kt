58b67b15bcfcc1ca3e56d84f0fd51bc270e9b89b
Venus <vega.sison.up@phinmaed.com>
2025-02-25 12:11:06 +0800

    venus commit
    login button click

--- app/src/main/java/com/example/taskconnectapp/LoginPage.kt
+++ app/src/main/java/com/example/taskconnectapp/LoginPage.kt
@@ -1,6 +1,8 @@
 package com.example.taskconnectapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,19 @@ class LoginPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login_page)
+
+        // Set padding for system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // If login button click it will go to homepage
+        val loginButton = findViewById<Button>(R.id.loginButton) // Ensure this ID matches the button in your XML
+        loginButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
