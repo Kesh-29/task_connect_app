d95d1faa1e2effca3a930e1c5b5f95d963503bf9
Patrick Cruz <flyerbek.xd0921@gmail.com>
2025-03-05 21:33:14 +0800

    d

--- app/src/main/java/com/example/taskconnectapp/PostingPageActivity.kt
+++ app/src/main/java/com/example/taskconnectapp/PostingPageActivity.kt
@@ -1,6 +1,7 @@
 package com.example.taskconnectapp
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,13 @@ class PostingPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_posting_page)
+
+        val backButton = findViewById<ImageView>(R.id.backButton)
+
+        backButton.setOnClickListener {
+            finish() // Go back to the previous activity
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
